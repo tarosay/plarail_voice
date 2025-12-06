c7134f5e5cb4b4e5467e617e2cb6d4c3e82e7d5c
Tarosa <tarosa.y@gmail.com>
2025-12-06 16:51:43 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ if (input.buttonIsPressed(Button.A)) {
 radio.sendNumber(0)
 let 回 = 0
 basic.forever(function () {
-    if (input.soundLevel() > 230) {
+    if (input.soundLevel() < 254 && input.soundLevel() > 220) {
         回 = 回 + 1
     } else {
         回 = 回 * 0.93
